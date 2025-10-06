04a7638b950f9c74bbc3610767f253e4242ba3a8
desibasa <desi.basa@gmail.com>
2025-10-06 13:37:31 +0200

    06.1

---
+++ archivos php/index.php
@@ -0,0 +1,24 @@
+
+<h1>
+
+    <?= "Mi primera app" ?>
+
+</h1>
+<?php
+
+    echo"Holamundo";
+
+    $name = "Miguel";
+
+    $edad = 30;
+
+    $verdadero = true;
+
+    // las variables en php no pueden comenzar con numero ni ponerle otro dolar al final o en medio
+    //php es de tipado dinamico (no hace falta decirle que tipo de variable es), debil y escalable ademas las variables pueden cambiar a lo largo del programa
+    //Es muy importante saber que php tiene tipado debil porque va a transformar los valores de las variables ejemplo:
+
+    $numero = 30 + "1";
+    echo $numero; //Esto va a dar 31 porque va a transformar el string a numero
+
+?>
