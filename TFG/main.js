3ea7c7483c79c2257186aa39c67de2792f7ffbaf
desibasa <desibasagrande@gmail.com>
2026-01-19 22:08:54 +0100

    commit 19

--- TFG/main.js
+++ TFG/main.js
@@ -1,22 +1,24 @@
 fetch("nav.html") //Busca nav.html
-    .then (response => response.text()) //Entonces transforma la respuesta a texto y guardala 
-    .then (data => { //guardala en datos y hazme lo siguiente
+    .then(response => response.text()) //Entonces transforma la respuesta a texto y guardala 
+    .then(data => { //guardala en datos y hazme lo siguiente
         document.getElementById("navegador").innerHTML = data; //buscame el elemento "navegador" y cambiale el contenido para que sea el texto que estaba guardado en data
+
+        const treslineas = document.querySelector(".treslineas");
+        const equis = document.querySelector(".equis");
+        const listaResponsive = document.getElementById("lista_responsive");
+        const hamburguesa = document.getElementById("hamburguesa");
+
+        // Evento click
+        hamburguesa.addEventListener('click', function () {
+            if (treslineas.style.display !== "none") {
+                treslineas.style.display = "none";
+                equis.style.display = "block";
+                listaResponsive.style.display = "flex";
+                listaResponsive.style.flexDirection = "column";
+            } else {
+                treslineas.style.display = "block";
+                equis.style.display = "none";
+                listaResponsive.style.display = "none";
+            }
+        });
     });
-
-    function aparecer(){
-
-        document.getElementById("hamburguesa").querySelectorAll.style.display = "flex";
-
-    }
-
-    let treslineas = document.getElementsByClassName("treslineas");
-    let equis = document.getElementsByClassName("equis");
-
-    function aparecer(){
-
-        if(equis.style.display="none"){
-                equis.style.display="flex";
-        }
-
-    }
